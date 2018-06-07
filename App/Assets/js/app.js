f12fe7b03941d22de7a0bde417b81d42e5f3e0d3
Kevyn Carlos <kevyncarlos@gmail.com>
2018-06-07 01:39:00 -0300

    foda

---
+++ App/Assets/js/app.js
@@ -0,0 +1,25 @@
+﻿window.Popper = require('popper.js').default;
+
+window.$ = window.jQuery = require("jquery");
+window.Highcharts = require('highcharts');
+require('highcharts/highcharts-3d')(Highcharts);
+window.dtPtBr = require('./datatables.ptBr');
+window.PNotify = require('pnotify');
+window.alertify = require('alertifyjs');
+
+PNotify.defaults.styling = 'bootstrap4';
+PNotify.defaults.icons = 'fontawesome4';
+
+require('bootstrap');
+require('jquery-mask-plugin');
+require('bootstrap-maxlength');
+require('select2');
+require('select2/dist/js/i18n/pt-BR');
+require('jquery-validation');
+
+$.extend(true, $.fn.dataTable.defaults, { language: dtPtBr, "ordering": false, responsive: true, theme: "bootstrap", "lengthMenu": [[25, 50, 100, 500, -1], [25, 50, 100, 500, "Todos"]], "pageLength": 50, destroy: true });
+$.fn.datetimepicker.Constructor.Default = $.extend({}, $.fn.datetimepicker.Constructor.Default, { locale: "pt-br" });
+$.fn.select2.defaults.set("theme", "bootstrap");
+$.validator.setDefaults({ errorClass: 'text-danger' });
+
+require('./custom');
---
+++ App/gulpfile.js
@@ -0,0 +1,41 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+var js = [
+    
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/highcharts/highcharts.min.js',
+    './node_modules/highcharts/highcharts-3d.min.js',
+    './node_modules/pnotify/dist/pnotify.js',
+    './node_modules/alertifyjs/build/*.js',
+    './node_modules/bootstrap/dist/bootstrap.min.js',
+    './node_modules/jquery-mask-plugin/jquery.mask.min.js',
+    './node_modules/bootstrap-maxlength/bootstrap-maxlength.min.js',
+    './node_modules/select2/dist/js/select2.min.js',
+    './node_modules/select2/dist/js/i18n/pt-BR.js',
+    './node_modules/jquery-validation/dist/jquery.validate.min.js',
+    './node_modules/jquery-validation/dist/localization/messages_pt_BR.js',
+    //'./node_modules/datatables.net/js/*.js',
+    //'./node_modules/datatables.net-bs4/js/*.js',
+    //'./node_modules/datatables.net-responsive-bs4/js/*.js',
+    './Assets/js/custom.js'
+];
+
+gulp.task('css', function () {
+    return gulp.src('./Assets/scss/app.scss')
+        .pipe(sass({ outputStyle: 'compressed', includePaths: ['node_modules'] }).on('error', sass.logError))
+        .pipe(rename('app.min.css'))
+        .pipe(gulp.dest('./wwwroot/css'))
+});
+
+gulp.task('js', function () {
+    return gulp.src(js)
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('./wwwroot/js'))
+});
+
+gulp.task('default', ['css', 'js']);
